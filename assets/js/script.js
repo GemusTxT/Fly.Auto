fd6d93c2ec7ef49eacbd548d4f55575e1f74de7e
GemusTxT <kirilloffwhite1335@gmail.com>
2024-09-25 04:33:03 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function closeNav() {
+    const nav = document.getElementById('nav');
+    nav.classList.remove('active');
+}
+
+function toggleNav() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
